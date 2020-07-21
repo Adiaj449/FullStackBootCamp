9a6d909879918ddf5169b7c7a07bf6ba4c84c181
Ajinkya Algonda <ajinkyasoftech449@gmail.com>
2020-07-21 20:52:44 +0530

    Modified code for compatible with NodeJS

--- Zen Class Day 1/MaxNumArray.js
+++ Zen Class Day 1/MaxNumArray.js
@@ -1,16 +1,3 @@
-const readline = require('readline');
-const inp = readline.createInterface({
-  input: process.stdin
-});
-const userInput = [];
-inp.on("line", (data) => {
- userInput.push(data);
-});
-inp.on("close", () => {
-
-//start-here
-//Your code goes here … replace this line with your code logic 
-
 let array = [4,2,1,5,6,2];
 let max = array[0];
 
@@ -21,8 +8,4 @@ for(let i=1;i<array.length;i++)
         max=array[i];
     }
 }
-
 console.log(max);
-
-//end-here
-});
